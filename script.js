a41813a205467fc75946167d2eb68e1cd208943c
flownotionstudios <141359638+flownotionstudios@users.noreply.github.com>
2023-08-03 15:04:46 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,80 @@
+const NOTION_DATABASE_ID = '7b050bafa16e4fabb6de7d502e8c5bcd';
+const NOTION_API_KEY = 'secret_UbCe5e7QwrpK8fadwOHDxBa1ydcY0HFy32kfUFyUKaD';
+
+let spinning = false;
+const displayedItem = document.getElementById('displayedItem');
+
+async function fetchNotionDatabase() {
+  try {
+    const response = await fetch(
+      `https://api.notion.com/v1/databases/${7b050bafa16e4fabb6de7d502e8c5bcd}/query`,
+      {
+        method: 'POST',
+        headers: {
+          'Authorization': `Bearer ${secret_UbCe5e7QwrpK8fadwOHDxBa1ydcY0HFy32kfUFyUKaD}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          // You can customize the query as needed
+        }),
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch data from Notion API');
+    }
+
+    const data = await response.json();
+    return data.results;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
+// Modify the spinWheel function to use Notion database items
+async function spinWheel() {
+  const notionDatabase = await fetchNotionDatabase();
+  if (notionDatabase.length === 0) {
+    alert('No items found in the Notion database');
+    return;
+  }
+
+  const randomIndex = Math.floor(Math.random() * notionDatabase.length);
+  displayedItem.textContent = notionDatabase[randomIndex].properties.Name.title[0].text.content;
+}
+
+// The rest of the code remains the same.
+// ...
+let spinning = false;
+const displayedItem = document.getElementById("displayedItem");
+
+function spinWheel() {
+  const randomIndex = Math.floor(Math.random() * notionDatabase.length);
+  displayedItem.textContent = notionDatabase[randomIndex];
+}
+
+function startSpinning() {
+  if (!spinning) {
+    spinning = true;
+    const spinInterval = 100; // Adjust this value for faster or slower spinning
+    const spinDuration = 2000; // Adjust this value for longer or shorter spinning time
+
+    let spinCount = 0;
+    const totalSpins = 20; // Adjust this value to control the number of spins
+
+    function spin() {
+      spinWheel();
+      spinCount++;
+
+      if (spinCount < totalSpins) {
+        setTimeout(spin, spinInterval);
+      } else {
+        spinning = false;
+        alert("Congratulations! You won: " + displayedItem.textContent);
+      }
+    }
+
+    setTimeout(spin, spinDuration);
+  }
+}
